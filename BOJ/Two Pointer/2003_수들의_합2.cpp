4b300757767a5776b02e45fdedf069a440bbc8cc
jessica9685 <49610085+jessica9685@users.noreply.github.com>
2021-03-12 18:20:03 +0900

    Create 2003_수들의_합2.cpp

---
+++ BOJ/Two Pointer/2003_수들의_합2.cpp
@@ -0,0 +1,45 @@
+// 2003_수들의_합2.cpp
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX 10000
+
+int n;
+long long m;
+int seq[MAX];
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) cin >> seq[i];
+	
+	int left = 0;
+	int right = 0;
+	long long sum = seq[0];
+	int cnt = 0;
+
+	while (right < n && left <= right) {
+		if (sum < m) {
+			right++;
+			if (right >= n) break;
+			sum += seq[right];
+		}
+		else if (sum == m) {
+			cnt++; // 경우의 수 +1
+			right++; // right 이동
+			sum += seq[right];
+		}
+		else { // sum > m
+			sum -= seq[left]; 
+			left++; // left 이동
+			if (left > right) { // 범위를 넘어가면
+				right = left; // right를 left로 초기화
+				if (left < n) sum = seq[left];
+			}
+		}
+	}
+
+	cout << cnt << endl;
+}

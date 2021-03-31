d057207e20b6b235ec327699139f7995c82fb5b5
jessica9685 <49610085+jessica9685@users.noreply.github.com>
2021-04-01 02:03:38 +0900

    Create 더_맵게.cpp

---
+++ 프로그래머스/Heap(힙)/더_맵게.cpp
@@ -0,0 +1,33 @@
+// 프로그래머스 : 힙(Heap) - 더_맵게.cpp
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    
+    // 오름차순 우선순위 큐
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int cnt = scoville.size(); // 총 음식 개수
+    for(auto s : scoville) pq.push(s);
+    
+    while(pq.size() > 1){
+        int s1 = pq.top();
+        pq.pop();
+        int s2 = pq.top();
+        pq.pop();
+        
+        int mixture = s1 + (s2 * 2);
+        pq.push(mixture);
+        answer++;
+        
+        if(pq.top() >= K) break;
+    }
+    
+    if(pq.top() < K) answer = -1;
+    
+    return answer;
+}

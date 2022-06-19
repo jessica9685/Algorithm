4712d369f177917d6503d3d0bc1c5ab13b9eb9b2
jessica9685 <49610085+jessica9685@users.noreply.github.com>
2022-06-19 18:26:44 +0900

    Update 1600_말이_되고픈_원숭이.cpp

--- BOJ/BFS/1600_말이_되고픈_원숭이.cpp
+++ BOJ/BFS/1600_말이_되고픈_원숭이.cpp
@@ -2,72 +2,84 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <bits/stdc++.h>
 using namespace std;
-#define MAX 200
+
+typedef struct state {
+	int y;
+	int x;
+	int cnt; // 이동 횟수
+	int k; // 말로 이동한 횟수
+};
 
 int K, n, m;
-int ground[MAX][MAX];
-bool visited[MAX][MAX][31] = { false, }; // 말로 k번 뛰었을 때 방문 표시
-int horse[8][2] = { {-1, -2}, {-2, -1}, {-2, 1}, {-1, 2}, {1, -2}, {2, -1}, {2, 1}, {1, 2} };
+int board[200][200];
 int monkey[4][2] = { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
+int horse[8][2] = { 
+	{-1, -2}, {-2, -1},
+	{-2, 1}, {-1, 2},
+	{1, -2}, {2, -1},
+	{2, 1}, {1, 2}
+};
 
 int bfs(int sy, int sx) {
-	// first: 현재 좌표, second: 말로 뛴 횟수, 전체 횟수
-	queue<pair<pair<int, int>, pair<int, int>>> q;
+	// 말로 k번 뛰었을 때 방문표시
+	bool visited[200][200][31] = { false, };
 	visited[sy][sx][0] = true;
-	q.push({ {sy, sx}, {0, 0} });
+	queue<state> q;
+	q.push({ sy, sx, 0, 0 });
 
 	while (!q.empty()) {
-		int y = q.front().first.first;
-		int x = q.front().first.second;
-		int k = q.front().second.first;
-		int cnt = q.front().second.second;
+		int y = q.front().y;
+		int x = q.front().x;
+		int cnt = q.front().cnt;
+		int k = q.front().k;
 		q.pop();
 
 		if (y == n - 1 && x == m - 1) return cnt;
 
-		// 말로 뛰기
+		// 말로 이동하기
 		if (k < K) {
 			for (int i = 0; i < 8; i++) {
 				int ny = y + horse[i][0];
 				int nx = x + horse[i][1];
 
-				// 범위 벗어나면 패스
 				if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-				// 장애물이거나 이미 방문했으면 패스
-				if (visited[ny][nx][k + 1] || ground[ny][nx] == 1) continue;
-				
+				// 이미 방문했거나 장애물 있으면 패스
+				if (visited[ny][nx][k + 1] || board[ny][nx] == 1) continue;
+
 				visited[ny][nx][k + 1] = true;
-				q.push({ {ny, nx}, {k + 1, cnt + 1} });
+				q.push({ ny, nx, cnt + 1, k + 1 });
 			}
 		}
 
-		// 그냥 걷기
+		// 원숭이로 이동하기
 		for (int i = 0; i < 4; i++) {
 			int ny = y + monkey[i][0];
 			int nx = x + monkey[i][1];
 
-			// 범위 벗어나면 패스
 			if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-			// 장애물이거나 이미 방문했으면 패스
-			if (visited[ny][nx][k] || ground[ny][nx] == 1) continue;
+			// 이미 방문했거나 장애물 있으면 패스
+			if (visited[ny][nx][k] || board[ny][nx] == 1) continue;
 			
 			visited[ny][nx][k] = true;
-			q.push({ {ny, nx}, {k, cnt + 1} });
+			q.push({ ny, nx, cnt + 1, k });
 		}
 	}
 
+	// 도착할 수 없으면 -1
 	return -1;
 }
 
 int main() {
 	scanf("%d", &K);
 	scanf("%d %d", &m, &n);
+
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			scanf("%d", &ground[i][j]);
+			scanf("%d", &board[i][j]);
 		}
 	}
-	int ans = bfs(0, 0);
-	printf("%d\n", ans);
+
+	int answer = bfs(0, 0);
+	printf("%d\n", answer);
 	return 0;
 }

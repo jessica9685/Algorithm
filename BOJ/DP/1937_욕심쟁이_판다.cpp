17d877a87f9ccbab631221144545c2cf03e95e71
jessica9685 <49610085+jessica9685@users.noreply.github.com>
2022-09-24 20:25:06 +0900

    Update 1937_욕심쟁이_판다.cpp

--- BOJ/DP/1937_욕심쟁이_판다.cpp
+++ BOJ/DP/1937_욕심쟁이_판다.cpp
@@ -1,50 +1,46 @@
 // 1937_욕심쟁이_판다.cpp
-#define _CRT_SECURE_NO_WARNINGS
 #include <bits/stdc++.h>
 using namespace std;
 #define MAX 500
 
 int n;
-int dy[4] = { 1, -1, 0, 0 };
-int dx[4] = { 0, 0, 1, -1 };
 int forest[MAX][MAX];
-int dp[MAX][MAX]; // dp[a][b]: (a, b)에서 시작했을 때 최대 방문 칸수
+int dp[MAX][MAX]; // dp[i][j]: i행 j열에서 시작했을 때 갈 수 있는 최대 칸 수
+int dy[4] = {1, -1, 0, 0};
+int dx[4] = {0, 0, 1, -1};
 
-int dfs(int y, int x) {
-	// dp값이 이미 존재하면 리턴
-	if (dp[y][x] != 0) return dp[y][x];
-	dp[y][x] = 1; // 초기화
+int dfs(int y, int x){
+    // 이미 값이 존재하면 종료
+    if(dp[y][x] != 0) return dp[y][x];
+    dp[y][x] = 1; // 초기값 == 1
 
-	for (int i = 0; i < 4; i++) {
-		int ny = y + dy[i];
-		int nx = x + dx[i];
+    for(int d = 0; d < 4; d++){
+        int ny = y + dy[d];
+        int nx = x  + dx[d];
 
-		// 범위 벗어나면 패스
-		if (ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
-		// 다음 칸 이동
-		if (forest[ny][nx] > forest[y][x]) {
-			dp[y][x] = max(dp[y][x], dfs(ny, nx) + 1);
-		}
-	}
+        if(ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
+        if(forest[y][x] < forest[ny][nx]){
+            dp[y][x] = max(dp[y][x], dfs(ny, nx) + 1);
+        }
+    }
 
-	return dp[y][x];
+    return dp[y][x];
 }
 
 int main() {
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			scanf("%d", &forest[i][j]);
-		}
-	}
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            scanf("%d", &forest[i][j]);
+        }
+    }
 
-	int ans = 0;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			ans = max(ans, dfs(i, j));
-		}
-	}
-
-	printf("%d\n", ans);
-	return 0;
+    int answer = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            answer = max(answer, dfs(i, j));
+        }
+    }
+    printf("%d\n", answer);
+    return 0;
 }

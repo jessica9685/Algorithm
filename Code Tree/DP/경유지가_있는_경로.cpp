263c23731364627d9a8cbc8b2db3a7badf8bd763
jessica9685 <49610085+jessica9685@users.noreply.github.com>
2023-04-17 23:13:36 +0900

    Update 경유지가_있는_경로.cpp

--- Code Tree/DP/경유지가_있는_경로.cpp
+++ Code Tree/DP/경유지가_있는_경로.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define MAX 16
 
 int n, m, k;
-int dp[MAX][MAX];
+int dp[MAX][MAX]; // dp[i][j] : 시작 위치부터 (i, j)까지 가는 경우의 수
 
 int find_path(int sy, int sx, int ey, int ex){
     for(int y = sy + 1; y <= ey; y++){
@@ -25,11 +25,12 @@ int main() {
     int answer = 0;
     if(k == 0) answer = find_path(1, 1, n, m);
     else{
+        // 경유지 위치 구하기
         int y = k / m;
         int x = k % m;
         if(x == 0) x = m;
         else y += 1;
-
+        
         answer = find_path(1, 1, y, x) * find_path(y, x, n, m);
     }
 

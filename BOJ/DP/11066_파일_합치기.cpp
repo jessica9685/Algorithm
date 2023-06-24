c9c691e4f118fb6b7be72096667c39ae94286daf
jessica9685 <49610085+jessica9685@users.noreply.github.com>
2023-06-25 04:47:53 +0900

    Update 11066_파일_합치기.cpp

--- BOJ/DP/11066_파일_합치기.cpp
+++ BOJ/DP/11066_파일_합치기.cpp
@@ -1,41 +1,49 @@
 // 11066_파일_합치기.cpp
-#define _CRT_SECURE_NO_WARNINGS
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-#define MAX 501
+#define MAX 500 + 1
 #define INF 987654321
 
 int T;
-int n; // 장 수
-int files[MAX]; // 각 파일의 비용
-int dp[MAX][MAX]; // dp[a][b]: a ~ b 구간까지의 최소 비용
-int sum[MAX]; // sum[a]: 1 ~ a번째까지 합
+int n;
+
+int files[MAX];
+int dp[MAX][MAX]; // dp[a][b] : a ~ b 구간의 최소비용
+int sum[MAX]; // sum[a] : 1 ~ a까지의 합
 
 int main() {
-	scanf("%d", &T);
-	while (T--) {
-		scanf("%d", &n);
-		for (int i = 1; i <= n; i++) {
-			scanf("%d", &files[i]);
-			sum[i] = sum[i - 1] + files[i];
-		}
+    freopen("input.txt", "r", stdin);
 
-		for (int i = 1; i <= n; i++) { // 범위의 크기
-			for (int j = 1; j <= n - i; j++) { // 범위 시작 부분
-				dp[j][j + i] = INF; // 초기값
-				
-				/*
-					dp[4][11]의 최소값
-					= dp[4][5] + dp[6][11] + (sum[11] - sum[3])
-					= dp[j][k] + dp[k + 1][j + i] + (sum[j + i] - sum[j - 1])
-				*/
-				for (int k = j; k < i + j; k++) {
-					dp[j][j + i] = min(dp[j][j + i],
-						dp[j][k] + dp[k + 1][j + i] + sum[j + i] - sum[j - 1]);
-				}
-			}
-		}
-		printf("%d\n", dp[1][n]);
-	}
-	return 0;
+    scanf("%d", &T);
+
+    while(T--){
+        scanf("%d", &n);
+
+        for(int i = 1; i <= n; i++){
+            scanf("%d", &files[i]);
+            sum[i] = sum[i - 1] + files[i];
+        }
+
+        for(int j = 1; j <= n; j++){
+            for(int i = 1; i <= n- j; i++){
+                dp[i][i + j] = INF;
+
+		/*
+			dp[4][11]의 최소값
+			= dp[4][5] + dp[6][11] + (sum[11] - sum[3])
+			= dp[i][k] + dp[k + 1][i + j] + (sum[i + j] - sum[i - 1])
+		*/
+                for(int k = i; k < i + j; k++){
+                    dp[i][i + j] = min(dp[i][i + j], 
+                                    dp[i][k] + dp[k + 1][i + j] + sum[i + j] - sum[i - 1]);
+                }
+            }
+        }
+        
+        printf("%d\n", dp[1][n]);
+    }
+
+    return 0;
 }

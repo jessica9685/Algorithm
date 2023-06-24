22f3188e7af94d72af140b9bda80ea19964367fe
jessica9685 <49610085+jessica9685@users.noreply.github.com>
2023-06-25 03:34:34 +0900

    Update 10217_KCM_Travel.cpp

--- BOJ/Dijkstra(다익스트라)/10217_KCM_Travel.cpp
+++ BOJ/Dijkstra(다익스트라)/10217_KCM_Travel.cpp
@@ -1,91 +1,104 @@
 // 10217_KCM_Travel.cpp
-#define _CRT_SECURE_NO_WARNINGS
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
 using namespace std;
 #define MAX 101
-#define INF 10000001
+#define INF 987654321
 
-typedef struct info {
-	int v; // 정점
-	int cost; // 비용
-	int time; // 시간
+struct Ticket{
+    int dst;
+    int cost;
+    int time;
 };
 
+struct comp{
+    bool operator()(Ticket a, Ticket b){
+        return a.time > b.time;
+    }
+};
+
+bool sort_comp(Ticket a, Ticket b){
+    return a.time < b.time;
+}
+
+int T; // test case
 int n, m, k;
-int answer;
-vector<vector<info>> ticket;
-int dist[MAX][10001]; // dist[정점][사용한 비용] = 걸린 시간
+int dist[MAX][10001]; // dist[도착지][비용] = 걸린 시간
 
-// 거리(시간) 오름차순 정렬
-struct cmp {
-	bool operator()(info a, info b) {
-		return a.time > b.time;
-	}
-};
+vector<Ticket> tickets[MAX];
 
-void dijkstra(int s) {
-	// dist 배열 초기화
-	for (int i = 1; i <= n; i++) { // 1 ~ n
-		for (int j = 0; j <= m; j++) { // 0 ~ m
-			dist[i][j] = INF;
-		}
-	}
+void dijkstra(int start){
+    priority_queue<Ticket, vector<Ticket>, comp> pq;
+    dist[start][0] = 0;
+    pq.push({start, 0, 0});
 
-	priority_queue<info, vector<info>, cmp> pq;
-	dist[s][0] = 0;
-	info start;
-	start.v = s;
-	start.cost = 0;
-	start.time = 0;
+    while(!pq.empty()){
+        Ticket cur = pq.top();
+        pq.pop();
 
-	pq.push(start);
+        if(dist[cur.dst][cur.cost] < cur.time) continue;
 
-	while (!pq.empty()) {
-		info cur = pq.top();
-		pq.pop();
+        for(Ticket t : tickets[cur.dst]){
+            Ticket next;
+            next.dst = t.dst;
+            next.cost = cur.cost + t.cost;
+            next.time = cur.time + t.time;
 
-		if (dist[cur.v][cur.cost] < start.cost) continue;
-		for (info t : ticket[cur.v]) {
-			info next;
-			next.v = t.v; // 다음 정점
-			next.cost = cur.cost + t.cost; // 비용
-			next.time = dist[cur.v][cur.cost] + t.time; // 걸린 시간
+            // 비용 초과하면 패스
+            if(next.cost > m) continue;
+            // 기존 시간이 더 짧으면 패스 
+            if(dist[next.dst][next.cost] <= next.time) continue;
 
-			// 최대 비용을 넘어가면 continue
-			if (next.cost > 10000) continue;
-			if (dist[next.v][next.cost] > next.time) {
-				dist[next.v][next.cost] = next.time;
-				pq.push(next);
-			}
-		}
-	}
+            dist[next.dst][next.cost] = next.time;
 
-	// n까지 가는데 걸리는 최소 거리 구하기
-	for (int i = 0; i <= m; i++) {
-		answer = min(answer, dist[n][i]);
-	}
+            // dist 갱신
+            for(int i = next.cost + 1; i <= m; i++){
+                if(dist[next.dst][i] <= next.time) break;
+                dist[next.dst][i] = next.time;
+            }
+            
+            pq.push(next);
+        }
+    }
 }
 
 int main() {
-	int T;
-	scanf("%d", &T);
-	while (T--) {
-		scanf("%d %d %d", &n, &m, &k);
-		ticket.clear();
-		ticket.resize(n + 1);
+    freopen("input.txt", "r", stdin);
 
-		for (int i = 0; i < k; i++) {
-			int u;
-			info t;
-			scanf("%d %d %d %d", &u, &t.v, &t.cost, &t.time);
-			ticket[u].push_back(t);
-		}
+    scanf("%d", &T);
 
-		answer = INF;
-		dijkstra(1);
-		if (answer != INF) printf("%d\n", answer);
-		else printf("Poor KCM\n");
-	}
+    while(T--){
+        scanf("%d %d %d", &n, &m, &k);
 
-	return 0;
+        // init
+        for(int i = 1; i <= n; i++){
+            tickets[i].clear();
+            for(int j = 0; j <= m; j++) dist[i][j] = INF;
+        }
+
+        for(int i = 0; i < k; i++){
+            int u, v, c, d;
+            scanf("%d %d %d %d", &u, &v, &c, &d);
+            tickets[u].push_back({v, c, d});
+        }
+
+        // 시간 오름차순 정렬
+        for(int i = 1; i <= n; i++){
+            sort(tickets[i].begin(), tickets[i].end(), sort_comp);
+        }
+
+        dijkstra(1);
+
+        int ans = INF;
+        for(int i = 0; i <= m; i++){
+            if(ans > dist[n][i]) ans = dist[n][i];
+        }
+
+        if(ans == INF) printf("Poor KCM\n");
+        else printf("%d\n", ans);
+    }
+
+    return 0;
 }
